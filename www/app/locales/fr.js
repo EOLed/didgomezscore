24f0d65d00d7e156b519ab4ef5ece7195b2b5e10
Amos Chan <amos.chan@chapps.org>
2012-08-09 22:45:50 -0400

    french translations

--- www/app/locales/fr.js
+++ www/app/locales/fr.js
@@ -1,9 +1,17 @@
 {
-	"Did Scott Gomez Score Last Game?": "Did Scott Gomez Score Last Game?",
-	"Et pis maintenant?": "Et pis maintenant?",
-	"Reminiscing...": "Reminiscing...",
-	"last.goal.desc": "last.goal.desc",
+	"Did Scott Gomez Score Last Game?": "Est-ce que Gomez a marqu&eacute; l'autre jour?",
+  "Did The Habs Score Last Game?": "Est-ce que les Canadiens ont marqu&eacute; l'autre jour?",
+	"Et pis maintenant?": "How about now?",
+  "Yes!": "Oui!",
+  "No.": "Non.",
+  "Show Me the Money...": "Crache le cash...",
+  "salary %s": "Les Canadiens de Montr&eacute;al ont d&eacute;ja pay&eacute; Scott Gomez %s depuis son dernier but.",
+  "too much": "trop",
+  "Congrats Gomez!!!": "Bravo Gomez!!!",
+  "salary.format": "##,###,###.00$ USD",
+	"Reminiscing...": "Je me souviens...",
+	"last.goal.desc": "Le 17 f&eacute;vrier, Scott Gomez a doubl&eacute; son nombre de buts total de la saison en avantage num&eacute;rique &agrave; la fin de la premi&egrave;re p&eacute;riode.",
 	"Anticipation...": "Anticipation...",
-	"countup.prefix": "countup.prefix",
-	"countup.suffix": "countup.suffix"
+  "countup.prefix": "Seulement {d<}{dn} {dl},{d>} {hn} {hl}, {mn} {ml} et {sn} {sl} {desc}",
+  "countup.suffix": "depuis le dernier but de <span id=\"name\">Gomez</span>..."
 }
