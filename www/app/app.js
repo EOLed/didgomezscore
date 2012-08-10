2d773e02efc2f9d05aaf116a1f8a9c1c8851da0b
Amos Chan <amos.chan@chapps.org>
2012-08-09 20:48:24 -0400

    externalized strings (english only)

--- www/app/app.js
+++ www/app/app.js
@@ -1,12 +1,16 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+  routes = require('./routes'),
+  http = require('http'),
+  path = require('path'),
+  i18n = require('i18n');
+
+i18n.configure({
+  locales: ['fr','en']
+});
 
 var app = express();
 
@@ -18,6 +22,7 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(i18n.init);
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -27,6 +32,11 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+
+app.locals({
+  __i: i18n.__,
+  __n: i18n.__n
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
