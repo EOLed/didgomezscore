9bffe290a6544fef5ca586390dfc4c1bb0ee22cb
Amos Chan <amos.chan@chapps.org>
2012-08-14 21:52:21 -0400

    Ads enabled via redis config

--- www/app/routes/index.js
+++ www/app/routes/index.js
@@ -16,23 +16,68 @@ exports.index = function(req, res){
 
   console.log('locale from cookie: ' + locale);
 
-  res.render('index', { otherLocale: _otherLocale(locale) });
+  _loadConfig(res, locale, _configLoaded);
 };
 
 exports.locale = function(req, res) {
   var locale = _locale(req.params.locale);
-
   res.setHeader('Set-Cookie', 'locale=' + locale);
-
   i18n.setLocale(locale);
 
-  res.render('index', { otherLocale: _otherLocale(locale) });
+  _loadConfig(res, locale, _configLoaded);
 };
+
+function _loadConfig(res, locale, callback) {
+  var ads = true, scored = false;
+
+  var redis = require('redis');
+  var redisClient = redis.createClient();
+  redisClient.on('error', function (err) {
+    console.error('There was an Error ' + err);
+  });
+
+  var multi = redisClient.multi();
+
+  console.log('multi: ' + multi);
+
+  multi.get('dgs:config:ads', function (err, reply) {
+    console.log("get ads");
+    if (err) return;
+
+    ads = reply == 'true' ? true : false;
+  });
+
+  multi.get('dgs:config:scored', function (err, reply) {
+    console.log("get scored");
+    if (err) return;
+
+    if (reply == 'true') {
+      reply = true;
+    } else if (reply == 'false') {
+      reply = false;
+    }
+
+    scored = reply;
+  });
+
+  multi.exec(function (err, replies) {
+    var config = { scored: scored, ads: ads };
+    console.log(config);
+    callback(res, locale, config);
+
+    if (err) return;
+    redisClient.quit();
+  });
+}
+
+function _configLoaded(res, locale, config) {
+  res.render('index', { otherLocale: _otherLocale(locale), config: config });
+}
 
 function _otherLocale(locale) {
   return locale != 'en' ? 'en' : 'fr';
 }
 
 function _locale(locale) {
-  return locale == 'en' ? locale : 'fr';
+  return locale == 'fr' ? locale : 'en';
 }
