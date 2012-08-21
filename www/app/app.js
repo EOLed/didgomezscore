96707a316e8e108d6a5523aa727fe99265fa097a
Amos Chan <amos.chan@chapps.org>
2012-08-21 19:35:59 -0400

    Fixed issue #2. Re-caches tweets every 30 seconds.

--- www/app/app.js
+++ www/app/app.js
@@ -41,4 +41,27 @@ app.locals({
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+  setInterval(function() {
+    var url = 'http://search.twitter.com/search.json?q=didgomezscore&rpp=12&include_entities=true&result_type=mixed';
+    http.get(url, function (res) {
+      var data = '';
+      res.on('data', function(chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function() {
+        console.log('data: ' + data);
+        var redis = require('redis');
+        var redisClient = redis.createClient();
+        redisClient.on('error', function (err) {
+          console.error('There was an Error ' + err);
+        });
+
+        redisClient.set('dgs:tweets', data, redis.print);
+        redisClient.quit(function(err, res) {
+          console.log('closing redis client.');
+        });
+      });
+    });
+  }, 30000);
 });
